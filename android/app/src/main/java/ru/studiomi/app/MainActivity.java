84a2c2c10aaf5df5f25fd1bb1baa7aa4ccc181f6
glazov92 <glazov92@users.noreply.github.com>
2026-08-24 12:35:39 +0300

    Add missing LayoutInflater import

--- android/app/src/main/java/ru/studiomi/app/MainActivity.java
+++ android/app/src/main/java/ru/studiomi/app/MainActivity.java
@@ -7,6 +7,7 @@ import android.graphics.Typeface;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.TypedValue;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.webkit.CookieManager;
 import android.webkit.ValueCallback;
