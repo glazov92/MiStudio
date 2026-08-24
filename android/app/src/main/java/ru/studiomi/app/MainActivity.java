59f088b223fe9d7891e6d039bb9a00190484f5c3
glazov92 <glazov92@users.noreply.github.com>
2026-08-24 14:11:27 +0300

    Add missing FrameLayout import

--- android/app/src/main/java/ru/studiomi/app/MainActivity.java
+++ android/app/src/main/java/ru/studiomi/app/MainActivity.java
@@ -16,6 +16,7 @@ import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
