19f2db3d2f6fdd07f8ee4637473e0a14c56092cb
glazov92 <glazov92@users.noreply.github.com>
2026-08-24 15:00:53 +0300

    Restore toolbar title strings + EditText import

--- android/app/src/main/java/ru/studiomi/app/MainActivity.java
+++ android/app/src/main/java/ru/studiomi/app/MainActivity.java
@@ -16,6 +16,7 @@ import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
